ebc2675f82fc2f9357faab9e7a3807b37e283be3
ReconX <transformtosecure@gmail.com>
2026-02-18 22:38:06 +0000

    solve 01-chai-order string basics challenge

--- src/01-chai-order.js
+++ src/01-chai-order.js
@@ -46,21 +46,37 @@
  *   hasSpecialIngredient("Elaichi Chai", "elaichi")  // => true
  */
 export function getChaiOrderLength(order) {
-  // Your code here
+  if (typeof order !== "string")
+    return(-1)
+  return (order.trim().length)
 }
 
 export function shoutChaiOrder(order) {
-  // Your code here
+  if (typeof order !== "string")
+    return ("")
+  else if (order.trim()=="")
+    return ("")
+return (order.trim().toUpperCase())
 }
 
 export function whisperChaiOrder(order) {
-  // Your code here
+  if (typeof order !== "string")
+    return ("")
+  else if (order.trim() == "")
+    return("")
+  return (order.trim().toLowerCase())
 }
 
 export function hasSpecialIngredient(order, ingredient) {
-  // Your code here
+  if (typeof order !== "string" || typeof ingredient !== "string")
+    return (false)
+return (order.toLowerCase().includes(ingredient.toLowerCase()))
 }
 
 export function getFirstAndLastChar(order) {
-  // Your code here
+  if (typeof order !== "string")
+    return null 
+  else if (order.trim() == "")
+    return null
+return {first:order.trim().charAt(0), last:order.trim().at(-1)}
 }
